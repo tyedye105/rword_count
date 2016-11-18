2fd600ca1e4706de5f006e5350348ca67c111413
Marty Kovach <tyedye105@yahoo.com>
2016-11-18 11:01:00 -0800

    Add passing behavior to find instances of the word regardless of case

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,16 +1,16 @@
 class String
-    define_method(:word_count) do |search_word|
-      words_array = self.split()
-      words_found = 0
-      words_array.each() do |word|
-        if word == search_word
+  define_method(:word_count) do |search_word|
+    words_array = self.downcase().split()
+    words_found = 0
+    words_array.each() do |word|
+      if word == search_word
           words_found = words_found + 1
+        end
+      end
+      if words_found > 0
+        "The word is repeated " + words_found.to_s + " time(s)"
+      else
+    "We couldn't find that word... Is it even in the sentence?"
     end
   end
-    if words_found > 0
-    "The word is repeated " + words_found.to_s + " time(s)"
-  else
-    "We couldn't find that word... Is it even in the sentence?"
-  end
-end
 end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -2,7 +2,12 @@ require('rspec')
 require('word_count')
 
   describe ("String#word_count") do
-    it("will take a sentence, and word from the user, and output the number of times it occurs in the sentence") do
+    it("Will output the number of instances of a word in a sentence regardless of case") do
       expect("this is a cat".word_count("cat")).to(eq("The word is repeated 1 time(s)"))
     end
+
+    it("Will output the number of instances of a word in a sentence regardless of case") do
+      expect("that is a Dog".word_count("dog")).to(eq("The word is repeated 1 time(s)"))
+    end
+
   end
