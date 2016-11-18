703752bdaea875b98aa82cb1bbdf1828b51e69fc
Marty Kovach <tyedye105@yahoo.com>
2016-11-18 10:44:42 -0800

    Add end message for when the method finds zero instances of the word in the sentence

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -5,9 +5,12 @@ class String
       words_array.each() do |word|
         if word == search_word
           words_found = words_found + 1
-        else
     end
   end
+    if words_found > 0
     "The word is repeated " + words_found.to_s + " time(s)"
+  else
+    "We couldn't find that word... Is it even in the sentence?"
   end
+end
 end
