4f9972264f5749ed89a417814eac7541a9c93ddc
Marty Kovach <tyedye105@yahoo.com>
2016-11-18 12:32:42 -0800

    Add log for counting all instances search word regardless case.

---
+++ mettest.rb
@@ -0,0 +1,14 @@
+class Array
+  define_method(:removePunc) do
+    punctuation_array = [",",".","?","!"]
+      punctuation_array.each() do |punc|
+        if self.include?(punc)
+          self.delete(punc)
+        else
+          self.join()
+        end
+        self.join()
+      end
+      self
+    end
+end
