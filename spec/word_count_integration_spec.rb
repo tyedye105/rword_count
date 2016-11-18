6c0493a9443ccfa20b5c36e050bf5f6747f9263a
Marty Kovach <tyedye105@yahoo.com>
2016-11-18 15:27:01 -0800

    Add sinatra framework, and passing integration spec test

---
+++ spec/word_count_integration_spec.rb
@@ -0,0 +1,21 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+
+
+describe 'the word_count path', :type=>:feature do
+    it('takes a sentence inputed by the user along with a word, and will output the number of times the word occured in the sentence.') do
+      visit('/')
+      fill_in('sentence', :with => 'A great thing, that does a thing, saving you the effort of doing that one thing')
+      fill_in('word_find', :with => 'thing')
+      click_button('Find your word!')
+      expect(page).to have_content('The word is repeated 3 time(s)')
+  end
+    it('takes usser back to the starting page when clicked') do
+      visit('/result')
+      click_button('Try it again!')
+      expect.(page).to get (:index)
+    end
+end
